fbb4374b53afb8a42f7cb8a6fed379d8a8b6b348
yaobarry <32972777+yaobarry@users.noreply.github.com>
2018-06-19 00:49:30 +0800

    Initial commit

---
+++ gqs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GqsConfig(AppConfig):
+    name = 'gqs'
---
+++ gqs/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+class PayList(models.Model):
+    pid = models.BigIntegerField()
+    payname = models.CharField(max_length=100)
+    status = models.BooleanField()
+    def __str__(self):
+        return self.payname
+    class Meta:
+        ordering = ['pid']
+
+class SubPayList(models.Model):
+    sid = models.BigIntegerField()
+    subpayname = models.CharField(max_length=100)
+    status = models.BooleanField()
+    def __str__(self):
+        return self.subpayname
+    class Meta:
+        ordering = ['sid']
+
+class CardBin(models.Model):
+    cardbin = models.CharField(max_length=10)
+    cardnolen =  models.IntegerField()
+    cardtype =  models.BooleanField()
+    status = models.BooleanField()
+    bankname = models.CharField(max_length=100)
+    bankabbname = models.CharField(max_length=10)
+    associations = models.CharField(max_length=100)
+    country = models.CharField(max_length=10)
+    def __str__(self):
+        return self.cardbin
+    class Meta:
+        ordering = ['cardbin']
+
