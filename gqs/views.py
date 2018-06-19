9ee291e64082a55b28e47b5192b71091d37ee259
yaobarry <32972777+yaobarry@users.noreply.github.com>
2018-06-20 00:46:22 +0800

    gqs v0.01
    gqs v0.01

--- gqs/views.py
+++ gqs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+from gqs.models import PayList,SubPayList,CardBin
 
 # Create your views here.
+
+def homeview(request):
+    return render_to_response('index.html')
